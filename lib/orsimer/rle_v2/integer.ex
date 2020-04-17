31a21f0cc0403363c878d81ae4ed9308517c6209
Brian Balser <brian.balser@gmail.com>
2020-04-17 14:46:14 -0400

    Now created row indexes for integer columns

--- lib/orsimer/rle_v2/integer.ex
+++ lib/orsimer/rle_v2/integer.ex
@@ -1,17 +1,84 @@
 defmodule Orsimer.RLEv2.Integer do
   alias Orsimer.RLEv2.Integer.{Delta, Direct}
 
-  @spec encode([integer()], boolean) :: binary
-  def encode(integers, signed? \\ false, buffer \\ <<>>)
-
-  def encode([], _signed?, buffer), do: buffer
-
-  def encode(integers, signed?, buffer) do
-    {binary, remaining} = Direct.encode(integers, signed?)
-    encode(remaining, signed?, buffer <> binary)
+  defmodule Row do
+    defstruct positions: [0, 0, 0],
+              stats: Orc.Proto.IntegerStatistics.new(minimum: nil, maximum: nil, sum: 0),
+              binary: <<>>,
+              values: 0
   end
 
-  @spec decode(binary, boolean()) :: [integer()]
+  @chunk_size 512
+  @row_size 10_000
+
+  @spec encode([integer()], boolean) :: binary
+  def encode(integers, signed? \\ false) do
+    rows =
+      integers
+      |> Enum.chunk_every(@chunk_size)
+      |> encode_chunks(signed?, 0, [%Row{}])
+
+    binary =
+      rows
+      |> Enum.map(&Map.get(&1, :binary))
+      |> Enum.join()
+
+    indexes =
+      rows
+      |> Enum.map(fn row ->
+        Orc.Proto.RowIndexEntry.new(
+          positions: row.positions,
+          statistics:
+            Orc.Proto.ColumnStatistics.new(
+              numberOfValues: row.values,
+              hasNull: false,
+              intStatistics: row.stats
+            )
+        )
+      end)
+
+    {binary, indexes}
+  end
+
+  defp encode_chunks([], _signed?, _count, rows), do: Enum.reverse(rows)
+
+  defp encode_chunks([chunk | tail], signed?, count, [previous_row | rows])
+       when count + length(chunk) > @row_size do
+    to_consume = @row_size - count
+    new_count = length(chunk) - to_consume
+
+    [_, previous_start, _] = previous_row.positions
+    new_start = previous_start + byte_size(previous_row.binary)
+
+    {prev_numbers, next_numbers} = Enum.split(chunk, to_consume)
+    previous_row =
+      previous_row
+      |> update_stats(prev_numbers)
+      |> update_count_of_values(to_consume)
+
+    new_row =
+      %Row{positions: [0, new_start, to_consume]}
+      |> update_binary(chunk, signed?)
+      |> update_stats(next_numbers)
+      |> update_count_of_values(new_count)
+
+    encode_chunks(tail, signed?, new_count, [new_row, previous_row | rows])
+  end
+
+  defp encode_chunks([chunk | tail], signed?, count, [row | rows]) do
+    length = length(chunk)
+    new_count = count + length
+
+    new_row =
+      row
+      |> update_stats(chunk)
+      |> update_binary(chunk, signed?)
+      |> update_count_of_values(length)
+
+    encode_chunks(tail, signed?, new_count, [new_row | rows])
+  end
+
+  @spec decode(binary, boolean(), list) :: [integer()]
   def decode(binary, signed? \\ false, buffer \\ [])
 
   def decode(<<>>, _signed?, buffer), do: List.flatten(buffer)
@@ -24,5 +91,36 @@ defmodule Orsimer.RLEv2.Integer do
   def decode(<<3::size(2), _::bitstring>> = binary, signed?, buffer) do
     {integers, remaining} = Delta.decode(binary, signed?)
     decode(remaining, signed?, [buffer | integers])
+  end
+
+  defp update_stats(%Row{} = row, integers) do
+    Map.update!(row, :stats, fn stats ->
+      %{
+        stats
+        | minimum: safe_min(stats.minimum, integers),
+          maximum: safe_max(stats.maximum, integers),
+          sum: stats.sum + Enum.sum(integers)
+      }
+    end)
+  end
+
+  defp safe_min(nil, []), do: nil
+  defp safe_min(nil, list), do: Enum.min(list)
+  defp safe_min(cur, []), do: cur
+  defp safe_min(cur, list), do: min(cur, Enum.min(list))
+
+  defp safe_max(nil, []), do: nil
+  defp safe_max(nil, list), do: Enum.max(list)
+  defp safe_max(cur, []), do: cur
+  defp safe_max(cur, list), do: max(cur, Enum.max(list))
+
+  defp update_binary(%Row{} = row, integers, signed?) do
+    {binary, []} = Direct.encode(integers, signed?)
+
+    %{row | binary: row.binary <> binary}
+  end
+
+  defp update_count_of_values(%Row{} = row, count) do
+    %{row | values: row.values + count}
   end
 end
--- test/orsimer/rle_v2/integer_test.ex
+++ test/orsimer/rle_v2/integer_test.ex
@@ -4,10 +4,41 @@ defmodule Orsimer.RLEv2.IntegerTest do
   test "will encode a list of integers in direct encoding" do
     integers = random_numbers(4367)
 
-    binary_stream = Orsimer.RLEv2.Integer.encode(integers, true)
+    {binary_stream, _} = Orsimer.RLEv2.Integer.encode(integers, true)
     decoded = Orsimer.RLEv2.Integer.decode(binary_stream, true)
 
     assert decoded == integers
+  end
+
+  test "encode will create row index entries" do
+    integers = random_numbers(22137)
+
+    {binary_stream, row_indexes} = Orsimer.RLEv2.Integer.encode(integers, true)
+
+    assert 3 == Enum.count(row_indexes)
+
+    [0, start_one, 0] = Enum.at(row_indexes, 0) |> Map.get(:positions)
+    [0, start_two, consume_two] = Enum.at(row_indexes, 1) |> Map.get(:positions)
+    [0, start_three, consume_three] = Enum.at(row_indexes, 2) |> Map.get(:positions)
+
+    first_group =
+      binary_part(binary_stream, start_one, start_two - start_one)
+      |> Orsimer.RLEv2.Integer.decode(true)
+
+    second_group =
+      binary_part(binary_stream, start_two, start_three - start_two)
+      |> Orsimer.RLEv2.Integer.decode(true)
+
+    third_group =
+      binary_part(binary_stream, start_three, byte_size(binary_stream) - start_three)
+      |> Orsimer.RLEv2.Integer.decode(true)
+
+    assert first_group ++ Enum.take(second_group, consume_two) == Enum.take(integers, 10_000)
+
+    assert Enum.drop(second_group, consume_two) ++ Enum.take(third_group, consume_three) ==
+             Enum.slice(integers, 10_000..19_999)
+
+    assert Enum.drop(third_group, consume_three) == Enum.drop(integers, 20_000)
   end
 
   defp random_numbers(n) do
